2a359a695b6b588e980f67bb93566953913da582
Pedro Cruz <pedro_cruz_1995@hotmail.com>
2019-12-14 17:27:40 +0000

    Almost there, made from scratch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "live-chat_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "message_me_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Ana", password: "password")
+User.create(username: "Matos", password: "password")
+User.create(username: "Pedro", password: "password")
+User.create(username: "Frodo", password: "password")
+User.create(username: "Gandalf", password: "password")
